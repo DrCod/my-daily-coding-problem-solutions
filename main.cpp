f5621bad6b8da780e72157fd1fe95e97208b4262
Ronny <polleronny@yahoo.com>
2019-06-16 09:13:21 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: dronny
+ *
+ * Created on June 16, 2019, 9:05 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
